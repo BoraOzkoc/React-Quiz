839e4493207dc96925c6de2d3f151c2c49ad41b1
Bora Özkoç <boraozkoc@hotmail.com>
2025-01-05 21:01:33 +0300

    Name updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import React from "react";
 import Data from "./fetchdata";
-import QuizTemplate from "./quiztemplate";
 
 export default function Home() {
   return (
